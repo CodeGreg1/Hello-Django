4c0fd09ae7a1ae5d2c5990b87ef70ae9dedf680b
GregWebDev <code@greggoodrem.co.uk>
2022-10-30 14:08:36 +0000

    CRUD for Todo list complete

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kn_uh40+4)!i*le11=t_97csf!o&q91z9j95(7xcyx)wan*9&t'
+SECRET_KEY = (
+    'django-insecure-kn_uh40+4)!i*le11=t_97csf!o&q91z9j95(7xcyx)wan*9&t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,16 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.MinimumLengthValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.CommonPasswordValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.NumericPasswordValidator'),
     },
 ]
 
@@ -124,4 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CSRF_TRUSTED_ORIGINS = {'https://8000-codegreg1-hellodjango-s5ukph68wir.ws-eu72.gitpod.io'}
+CSRF_TRUSTED_ORIGINS = (
+    {'https://8000-codegreg1-hellodjango-s5ukph68wir.ws-eu73.gitpod.io'})
